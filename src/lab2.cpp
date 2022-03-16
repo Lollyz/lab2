ecd4a6fc8547e51f294a4216df4841962b004807
Lollyz <lorenzo.farinea1@gmail.com>
2022-03-16 19:02:36 +0100

    added task 1 and bugged task 2

--- src/lab2.cpp
+++ src/lab2.cpp
@@ -1,12 +1,28 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "functions.h"
 
 int main(int argc, char** argv) {
-    std::cout << "\nTest\n";
+
+    // TASK 1
+
     cv::Mat img= cv::imread(argv[1]);
-    cv::namedWindow("Example 1");
-    cv::imshow("Example 1", img);
+    cv::namedWindow("Original Image", cv::WINDOW_NORMAL);
+    cv::imshow("Original Image", img);
+
+    cv::Mat gray_img;
+    cv::cvtColor(img, gray_img,  cv::COLOR_BGR2GRAY);
+
+    cv::namedWindow("Gray Image", cv::WINDOW_NORMAL);
+    cv::imshow("Gray Image", gray_img);
     cv::waitKey(0);
+
+    cv::imwrite("gray_image.jpg",gray_img);
     
+    // TASK 2
+
+    cv::Mat max = img.clone();
+    max_filter(max, 3);
     return 0;
     }
